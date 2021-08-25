2d45f49a0c24ed111f16f53af6532097596bc0b2
Kendall Martinez <kendallmartinezcarvajal4@gmail.com>
2021-08-24 20:54:54 -0600

    Implementacion de comando para ejecutar y mejoramiento del codigo

--- main_code/main.cpp
+++ main_code/main.cpp
@@ -9,44 +9,71 @@
 #include <algorithm>
 using namespace std;
 
-void insertionSort(vector<int> &arr, int indice){
+void insertionSort(vector<int> &lista_datos){
     int i, key, j;
+    int indice=lista_datos.size();
     for(i=1; i<indice;i++){
         
-        key= arr[i];
+        key= lista_datos[i];
 
         j=i-1;
 
-        while (j>=0 && arr[j]>key){
-            arr[j+1]=arr[j];
+        while (j>=0 && lista_datos[j]>key){
+            lista_datos[j+1]=lista_datos[j];
             j = j-1;
         }
-        arr[j+1] = key;
+        lista_datos[j+1] = key;
     }
 }
 
 
+/*
+*leer_archivo
+*Este metodo se encarga recibir un string con el nombre del archivo a cargar o leer asi como
+*de escribir el los datos leidos en un vector
+*@param archivo_entrada corresponde al nombre del archivo a leer
+*/
+void leer_archivo(string nombre_archivo, vector<int> &lista_datos){
+ifstream archivo_entrada(nombre_archivo);
+char delimitador=',';
+string linea;
+while (getline(archivo_entrada,linea,delimitador)){
+    int numero_leido=stoi(linea,nullptr);
+    lista_datos.push_back(numero_leido);
+}
+archivo_entrada.close();
+}
 
-int main(){
-    ifstream archivo_entrada("archivo.txt");
-    char delimitador=',';
-    string linea;
-    vector<int> array;
-    while (getline(archivo_entrada,linea,delimitador)){
-        int numero=stoi(linea,nullptr);
-        array.push_back(numero);
-    }
-    archivo_entrada.close();
-    int indice=array.size();
-    insertionSort(array,indice);
-    ofstream archivo_salida("archivo_resultado.txt");
+void obtener_nombre_entrada_salida(string &cadena, string &nombre_archivo, string &nombre_salida){
+int primer_delimitador = cadena.find("<");
+string cadena2 = cadena.substr(primer_delimitador+1);
+int segundo_delimitador=cadena2.find(">");
+nombre_archivo=cadena2.substr(0,segundo_delimitador);
+primer_delimitador = cadena2.find("<");
+cadena = cadena2.substr(primer_delimitador+1);
+segundo_delimitador=cadena.find(">");
+nombre_salida=cadena.substr(0,segundo_delimitador);
+}
+
+void escribir_archivo(string &nombre_salida, vector<int> &lista_datos){
+    int indice=lista_datos.size();
+    ofstream archivo_salida(nombre_salida);
     for (int i = 0; i < indice; i++)
     {
-        archivo_salida << array[i] << ",";
+        archivo_salida << lista_datos[i] << ",";
     }
     archivo_salida.close();
-    //cout << indice << " Valor del indice" << endl;
-    //cout << array[0] << endl;
-    return 0;
 }
 
+int main(){
+    ///Entrada del comando para funcionar
+    string Entrada, nombre_archivo, nombre_salida;
+    vector<int> lista_datos;
+    getline(cin,Entrada);
+    obtener_nombre_entrada_salida(Entrada, nombre_archivo, nombre_salida);
+    leer_archivo(nombre_archivo,lista_datos);
+    insertionSort(lista_datos);
+    escribir_archivo(nombre_salida,lista_datos);
+
+    return 0;
+}
